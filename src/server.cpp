c82e2300f8868f3e6a4d9ee71d4020f9f3fb9bf3
nezhov <nikita.ejov2012@yandex.ru>
2021-01-03 11:35:48 +0300

    server lib completed

--- src/server.cpp
+++ src/server.cpp
@@ -15,6 +15,52 @@ bool ServerUp(int port){
     return true;
 }
 
+void SendMessages(zmqpp::socket &socketMessages, int id){
+    auto it = messageQueue.begin();
+    auto end = messageQueue.end();
+    static char c;
+    static std::string buf;
+    while(it != end){
+        if((*it).id == id){
+            std::ifstream in((*it).filename);
+            c = 0;
+            while(c != EOF){
+                int count = 0;
+                while(count < 256){
+                    c = (char)in.get();
+                    if(c != EOF) {
+                        buf += c;
+                    }
+                    else {
+                        break;
+                    }
+                    ++count;
+                }
+                if(c != EOF) {
+                    c = 0;
+                }
+                std::string message = std::to_string((*it).second_id) + " " + buf;
+                SendData(message, socketMessages);
+            }
+        }
+        ++it;
+    }
+}
+
+int BindSocket(zmqpp::socket &socketMessages){
+    int port = 3000;
+    while (true) {
+        try {
+            socket.bind(host+std::to_string(port));
+            break;
+        } catch(zmqpp::zmq_internal_exception& e) {
+            ++port;
+        }
+    }
+    return port;
+}
+
+
 std::string ReceiveRequest(){
     zmqpp::message message;
     try {
@@ -27,7 +73,7 @@ std::string ReceiveRequest(){
     return request;
 }
 
-void SendRequest(std::string &messageString){
+void SendData(std::string &messageString, zmqpp::socket &socket){
     zmqpp::message message;
     message >> messageString;
     try {
@@ -35,6 +81,27 @@ void SendRequest(std::string &messageString){
     } catch(zmqpp::zmq_internal_exception& e) {
         message = false;
     }
+}
+
+void SendData(std::string &messageString){
+    zmqpp::message message;
+    message >> messageString;
+    try {
+        socket.send(message);
+    } catch(zmqpp::zmq_internal_exception& e) {
+        message = false;
+    }
+}
+
+void DeleteMessage(){
+    messageId buf = messageQueue.front();
+    try{
+        remove(buf.filename.c_str());
+    }
+    catch(...){
+        std::cout << "Some error!" << std::endl;
+    }
+    messageQueue.pop_front();
 }
 
 void SendReply(std::ofstream log){
@@ -47,8 +114,8 @@ void SendReply(std::ofstream log){
     static std::string message;
     static std::string filename;
     in >> type;
+    in >> id;
     if(type == Send){
-        in >> id;
         c = 0;
         while (c != EOF){
             c = (char)in.get();
@@ -57,25 +124,39 @@ void SendReply(std::ofstream log){
         }
         c = 0;
         filename = "messages/" + std::to_string(id) + "_" + std::to_string(messageIndex) + ".txt";
-        auto *uniqueId = new messageId(id, filename);
-        messageQueue.push(*uniqueId);
+        auto *uniqueId = new messageId(id, messageIndex, filename);
+        messageQueue.push_back(*uniqueId);
         std::ofstream out(filename, std::ios::out);
         out << id << " " << messageIndex << "\n" << message;
         ++messageIndex;
+        ++queueSize;
+        if(queueSize == maxSize){
+            DeleteMessage();
+            --queueSize;
+        }
+        return;
     }
     if(type == BigSend){
-        in >> id;
         in >> secondId;
         if(secondId == -1){
             secondId = messageIndex;
 
             filename = "messages/" + std::to_string(id) + "_" + std::to_string(secondId) + ".txt";
-            std::ofstream out(filename, std::ios::out);
+            std::ofstream out(filename);
             out << id << " " << secondId << "\n";
 
+            auto *uniqueId = new messageId(id, messageIndex, filename);
+            messageQueue.push_back(*uniqueId);
+
             std::string send = std::to_string(messageIndex);
-            SendRequest(send);
+            SendData(send);
             ++messageIndex;
+            ++queueSize;
+            if(queueSize == maxSize){
+                DeleteMessage();
+                --queueSize;
+            }
+            return;
         }
         else{
             filename = "messages/" + std::to_string(id) + "_" + std::to_string(secondId) + ".txt";
@@ -89,7 +170,15 @@ void SendReply(std::ofstream log){
             c = 0;
             out << message;
             out.close();
+            return;
         }
+    }
+    if(type == Receive){
+        zmqpp::socket socketMessages(context, zmqpp::socket_type::push);
+        int port = BindSocket(socketMessages);
+        std::string response = std::to_string(port);
+        SendData(response);
+        SendMessages(socket, id);
     }
 }
 /*std::string recieve_msg(zmqpp::socket& socket) {
--- src/server.h
+++ src/server.h
@@ -9,7 +9,8 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
-#include <queue>
+#include <cstdio>
+#include <list>
 enum Operation{
     Send, //0
     BigSend, //1
@@ -17,17 +18,28 @@ enum Operation{
 };
 struct messageId{
     int id;
+    int second_id;
     std::string filename;
-    messageId(int id_, std::string filename_){
+    messageId(int id_, int second_id_, std::string filename_){
         this->id = id_;
+        this->second_id = second_id_;
         this->filename = filename_;
     }
 };
+
+static int maxSize = 1000;
+static int queueSize = 0;
 static int messageIndex = 0;
-static std::queue<messageId> messageQueue;
+static std::list<messageId> messageQueue;
 static zmqpp::context context;
 static zmqpp::socket socket(context, zmqpp::socket_type::rep);
 const std::string host = "tcp://127.0.0.1:";
 bool ServerUp(int port);
+void SendMessages(zmqpp::socket &socketMessages, int id);
+void SendReply(std::ofstream log);
 std::string ReceiveRequest();
+void SendData(std::string &messageString);
+void SendData(std::string &messageString, zmqpp::socket &socket);
+int BindSocket(zmqpp::socket &socketMessages);
+void DeleteMessage();
 #endif //KP_OS_SERVER_EXEC_H
